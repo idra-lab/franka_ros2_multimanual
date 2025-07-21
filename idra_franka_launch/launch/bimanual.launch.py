8f315b395be15c48b882d6e42ffee8208c15ce05
Moska002 <ale.moska002@gmail.com>
2025-07-21 09:39:50 +0200

    REMOVE Spawning of franka gripper

--- idra_franka_launch/launch/bimanual.launch.py
+++ idra_franka_launch/launch/bimanual.launch.py
@@ -242,14 +242,14 @@ def generate_launch_description():
     controller_left = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['joint_velocity_left', '--controller-manager', '/controller_manager'],
+        arguments=['cartesian_velocity_left', '--controller-manager', '/controller_manager'],
         output='screen'
     )
 
     controller_right = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['joint_velocity_right', '--controller-manager', '/controller_manager'],
+        arguments=['cartesian_velocity_right', '--controller-manager', '/controller_manager'],
         output='screen'
     )
 
@@ -323,8 +323,8 @@ def generate_launch_description():
         ),
 
         robot_description_dependent_nodes_spawner_opaque_function,
-        robot_gripper_left,
-        robot_gripper_right,
+        # robot_gripper_left,
+        # robot_gripper_right,
 
         # Launch Gazebo
         IncludeLaunchDescription(
