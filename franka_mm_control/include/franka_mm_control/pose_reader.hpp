b83d483fd3e33e34bfd8b2d9892b9d870f4f3075
Moska002 <ale.moska002@gmail.com>
2025-07-25 13:36:25 +0200

    ADD Documentation

--- franka_mm_control/include/franka_mm_control/pose_reader.hpp
+++ franka_mm_control/include/franka_mm_control/pose_reader.hpp
@@ -23,14 +23,63 @@
 #include <hardware_interface/types/hardware_interface_type_values.hpp>
 #include <controller_interface/controller_interface.hpp>
 
+/**
+ * This controller reads the values inside Franka Robots pose interfaces, 
+ * concatenates as double array and publishes them inside 
+ * the /current_pose topic.
+ * 
+ * This controller has an input, robot_names, that is a list of 
+ * robot names that needs to be searched.
+ * 
+ * The output is expresesd as homogeneous transfomrations matrices in 
+ * column-major format. The vector as length equal to 16 * robot_number
+ * 
+ * @author Alessandro Moscatelli
+ */
 class PoseReader : public controller_interface::ControllerInterface {
 public:
     PoseReader() = default;
 
+    /**
+     * Initializes robot names vector from controller parameter and initialises /current_pose publisher 
+     * 
+     * @returns Success if all the setup went smoothly.
+     */
     controller_interface::CallbackReturn on_init() override;
+
+    /**
+     * Loads the command interface configuration. No command interfaces are used in this controller.
+     * 
+     * @returns Object containing the requested interfaces
+     */
     controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+    /**
+     * Loads the state interface configuration. 
+     * State interfaces must are in the format <robot_name>_i/cartesian_pose_command, 
+     * where i is [0, 15] rapresenting the indexes of the transformation matrix.
+     * 
+     * @returns Object containing the requested interfaces
+     */
     controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+    /**
+     * Does nothigh.
+     * 
+     * @param previous_state ROS lifecycle previous state
+     * 
+     * @return Status of the function after the call
+     */
     controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+
+    /**
+    * Reads the values from the state interfaces and publishes them inside the topic
+    *
+    * @param time   Time when the function was called
+    * @param period Duration difference since the last call
+    *
+    * @return Status of the function after the call
+    */
     controller_interface::return_type update(const rclcpp::Time&, const rclcpp::Duration&) override;
 
     /**
@@ -47,7 +96,14 @@ private:
     */
     rclcpp::Logger logger = rclcpp::get_logger("pose_reader");
 
+    /**
+     * Robot names
+     */
     std::vector<std::string> robot_names;
+
+    /**
+     * Publisher object where poses will be outputted
+     */
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr publisher;
 };
 
